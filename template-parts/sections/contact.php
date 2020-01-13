03979aa2bc9f7e17f3a29fca6fa7edbac5e184d2
Anisuzzaman Titas <titas@mazegeek.com>
2020-01-13 17:06:50 +0600

    portfolio section done

--- template-parts/sections/contact.php
+++ template-parts/sections/contact.php
@@ -1,48 +1,85 @@
-<section class="analysis-section">
+<section class="subscribe-area">
     <div class="container">
         <div class="row align-items-center">
-            <div class="col-lg-6 col-md-12">
-                <div class="analysis-image">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/analysis-image.png" alt="image">
+            <div class="col-lg-4 col-md-12">
+                <div class="contact-box">
+                    <div class="icon">
+                        <i class="fa fa-phone"></i>
+                    </div>
+
+                    <div class="content">
+                        <h4>Phone / Fax</h4>
+                        <p><a href="">(+021) 245522455</a></p>
+                        <p><a href="#">(+000) 245522455</a></p>
+                    </div>
                 </div>
             </div>
 
-            <div class="col-lg-6 col-md-12">
-                <div class="analysis-area-content">
-                    <span>Analysis</span>
-                    <h3>Get Free SEO Analysis</h3>
-                    <strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</strong>
+            <div class="col-lg-4 col-md-12">
+                <div class="contact-box">
+                    <div class="icon">
+                        <i class="fas fa-envelope"></i>
+                    </div>
+
+                    <div class="content">
+                        <h4>E-mail</h4>
+                        <p><a href="#">hello@freelancershub.com </a></p>
+                        <p><a href="#">support@freelancershub.com</a></p>
+                    </div>
                 </div>
+            </div>
 
-                <form class="contactForm">
+            <div class="col-lg-4 col-md-12">
+                <div class="contact-box">
+                    <div class="icon">
+                        <i class="fa fa-map-marker"></i>
+                    </div>
+
+                    <div class="content">
+                        <h4>Location</h4>
+                        <p>2750 Quadra Street , Park Area, <span>Victoria, Canada.</span></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6">
+                <div class="contact-text">
+                    <h3>Get in Touch</h3>
+                    <p>Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam tempus magna vel turpis pharetra dictum.</p>
+                    <p>Sed blandit tempus purus, sed sodales leo rutrum vel. Nam vulputate ipsum ac est congue, eget commodo magna lobortis.</p>
+                </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6">
+                <form id="contactForm">
                     <div class="row">
-                        <div class="col-lg-6 col-md-6 col-sm-6">
-                            <div class="form-group">
-                                <input type="text" name="name" id="name" class="form-control" placeholder="Your Name*">
+                        <div class="col-lg-6 col-md-12">
+                            <div class="form-group form-bg-white">
+                                <input type="text" class="form-control" placeholder="Name">
                             </div>
                         </div>
 
-                        <div class="col-lg-6 col-md-6 col-sm-6">
-                            <div class="form-group">
-                                <input type="text" name="text" id="text" class="form-control" placeholder="Email*">
+                        <div class="col-lg-6 col-md-12">
+                            <div class="form-group form-bg-white">
+                                <input type="email" class="form-control" placeholder="Email">
                             </div>
                         </div>
 
-                        <div class="col-lg-6 col-md-6 col-sm-6">
-                            <div class="form-group">
-                                <input type="text" name="name" id="phone" class="form-control" placeholder="Phone*">
+                        <div class="col-lg-12 col-md-12">
+                            <div class="form-group form-bg-white">
+                                <input type="text" class="form-control" placeholder="Subject">
                             </div>
                         </div>
 
-                        <div class="col-lg-6 col-md-6 col-sm-6">
-                            <div class="form-group">
-                                <input type="text" name="name" id="website" class="form-control" placeholder="Website*">
+                        <div class="col-lg-12 col-md-12">
+                            <div class="form-group form-bg-white">
+                                <textarea name="message" class="form-control" placeholder="Your Message"></textarea>
                             </div>
                         </div>
 
                         <div class="col-lg-12 col-md-12">
                             <div class="send-btn">
-                                <button type="submit" class="default-btn-one">Send Message</button>
+                                <button type="submit" class="send-btn-one">Send Message</button>
                             </div>
                         </div>
                     </div>
--- template-parts/sections/projects.php
+++ template-parts/sections/projects.php
@@ -17,84 +17,50 @@ $freelancershub_reports_subheading = $freelancershub_section_meta['freelancershu
 
         <ul class="filter-menu">
             <li class="filter active" data-filter="all">All</li>
-            <li class="filter" data-filter=".branding">Branding</li>
-            <li class="filter" data-filter=".marketting">Marketing</li>
-            <li class="filter" data-filter=".seo">SEO</li>
-            <li class="filter" data-filter=".web">Web</li>
+            <?php
+            $terms = get_terms("category"); // get all categories, but you can use any taxonomy
+            $count = count($terms); //How many are they?
+            if ( $count > 0 ){  //If there are more than 0 terms
+                foreach ( $terms as $term ) {  //for each term:
+                    echo "<li class=\"filter\" data-filter='.".$term->slug."'>" . $term->name . "</li>\n";
+                }
+            }
+            ?>
         </ul>
 
         <div id="Container" class="row">
-            <div class="col-lg-4 col-md-6 mix branding">
+            <?php
+            $args = array(
+                'post_type'           => 'portfolio',
+                'posts_per_page'      => 9,
+            );
+            $query = new WP_Query( $args );
+            if ($query->have_posts() ):
+            while ( $query->have_posts() ) :
+            $query->the_post();
+            $termsArray = get_the_terms( $post->ID, "category" );  //Get the terms for this particular item
+            $termsString = ""; //initialize the string that will contain the terms
+            foreach ( $termsArray as $term ) { // for each term
+                $termsString .= $term->slug.' '; //create a string that has all the slugs
+            }
+            ?>
+            <div class="col-lg-4 col-md-6 mix <?php echo $termsString; ?>">
                 <div class="single-project-box">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/project/1.jpg" alt="image">
-
+                    <?php if ( has_post_thumbnail() ) {
+                        the_post_thumbnail();
+                    } ?>
                     <div class="project-hover-content">
                         <h3>
-                            <a href="#"> Search Enginee Optimization</a>
+                            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
                         </h3>
                     </div>
                 </div>
             </div>
-
-            <div class="col-lg-4 col-md-6 mix marketting">
-                <div class="single-project-box">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/project/2.jpg" alt="image">
-
-                    <div class="project-hover-content">
-                        <h3>
-                            <a href="#"> Search Enginee Optimization</a>
-                        </h3>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 mix seo">
-                <div class="single-project-box">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/project/3.jpg" alt="image">
-
-                    <div class="project-hover-content">
-                        <h3>
-                            <a href="#"> Search Enginee Optimization</a>
-                        </h3>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 mix web">
-                <div class="single-project-box">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/project/4.jpg" alt="image">
-
-                    <div class="project-hover-content">
-                        <h3>
-                            <a href="#"> Search Enginee Optimization</a>
-                        </h3>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 mix branding">
-                <div class="single-project-box">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/project/5.jpg" alt="image">
-
-                    <div class="project-hover-content">
-                        <h3>
-                            <a href="#"> Search Enginee Optimization</a>
-                        </h3>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 mix marketting">
-                <div class="single-project-box">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/project/6.jpg" alt="image">
-
-                    <div class="project-hover-content">
-                        <h3>
-                            <a href="#"> Search Enginee Optimization</a>
-                        </h3>
-                    </div>
-                </div>
-            </div>
+            <?php
+            endwhile;
+            endif;
+            wp_reset_postdata();
+            ?>
         </div>
     </div>
 </section>
